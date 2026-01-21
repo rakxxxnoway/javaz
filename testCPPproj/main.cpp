b4df9976e2f4fc9aa41e14c331289673a7f5ceb5
rakxxxnoway <rakxxxnoway@yandex.ru>
2026-01-21 12:07:31 +0100

    Release version 1.0.0
    Added C/C++ support

---
+++ testCPPproj/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * This simple program written by rNtR
+ * All rights reserved 2025 - ... ©
+*/
+#include <iostream>
+
+int main(void)
+{
+    // code
+
+    return 0;
+}
---
+++ testCproj/main.c
@@ -0,0 +1,12 @@
+/*
+ * This simple program written by rNtR
+ * All rights reserved 2025 - ... ©
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    // code
+
+    return 0;
+}
